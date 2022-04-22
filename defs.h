3f3d97d5afe1c45d79c8aff301942bbfd48cbccc
BetterCodings <smjoe030302@gmail.com>
2022-04-22 22:04:34 +0900

    file upload

--- defs.h
+++ defs.h
@@ -103,6 +103,7 @@ int             pipewrite(struct pipe*, char*, int);
 
 //PAGEBREAK: 16
 // proc.c
+int             ps(void);
 int             cpuid(void);
 void            exit(void);
 int             fork(void);
